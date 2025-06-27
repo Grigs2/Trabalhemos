fd183f54f5959c989a3d828c354914b8668576a9
Otavio Grigolon <otaviogrigolon@gmail.com>
2025-06-26 22:09:12 -0300

    Projeto iniciado com as dependencias padrões da aula de PAOO alem das configurações do banco de dados no arquivo application.properties

---
+++ src/main/java/com/example/Trabalhemos/TrabalhemosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trabalhemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Trabalhemos/TrabalhemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trabalhemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
